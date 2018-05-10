d8d275222d133fd09c535b80f452b88eaf3423ee
geronimoooooooo <geronimoooooooo@gmail.com>
2018-05-11 01:27:26 +0200

    Update pipes.ts

--- ng_ts/pipes.ts
+++ ng_ts/pipes.ts
@@ -1,1 +1,14 @@
 {{ userName | uppercase }}
+------
+<pre>{{myform.value | json}}</pre>
+//prints out
+{
+"name": {
+"firstName": null,
+"lastName": null
+},
+"email": null,
+"password": null,
+"language": null
+}
+--------------------------
