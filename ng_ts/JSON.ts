964020a56b5874a01d29fdd161ef473d0707c5ae
geronimoooooooo <geronimoooooooo@gmail.com>
2018-06-12 14:37:45 +0200

    Update JSON.ts

--- ng_ts/JSON.ts
+++ ng_ts/JSON.ts
@@ -4,6 +4,17 @@
 ---------------------------
  return JSON.parse(JSON.stringify(object));
 ---------------------------
+How to parse JSON string in Typescript
+https://stackoverflow.com/questions/38688822/how-to-parse-json-string-in-typescript
+
+interface MyObj {
+    myString: string;
+    myNumber: number;
+}
+
+let obj: MyObj = JSON.parse('{ "myString": "string", "myNumber": 4 }');
+console.log(obj.myString);
+console.log(obj.myNumber);
 ---------------------------
 ---------------------------
 ---------------------------
