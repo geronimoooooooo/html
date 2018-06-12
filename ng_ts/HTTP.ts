4b9de25c8028a59b75c188afb5ac46ba71c4b8ba
geronimoooooooo <geronimoooooooo@gmail.com>
2018-06-12 16:52:34 +0200

    Update HTTP.ts

--- ng_ts/HTTP.ts
+++ ng_ts/HTTP.ts
@@ -1,12 +1,16 @@
 https://www.concretepage.com/angular-2/angular-httpclient-get-example
 ---------------------------------------------
-    let url="https://gist.githubusercontent.com/geronimoooooooo/aa0da95fd7c0bf7956d5ad5a9ac9491e/raw/6b80b26853514f67d59bef63a6f9f5cc6384c6e9/q1.json";
-   
-    this.http.get(url, {responseType: 'json'}).subscribe(data => {
+    let url="https://gist.githubusercontent.com/geronimoooooooo/aa0da95fd7c0bf7956d5ad5a9ac9491e/raw/ea2d976eed25c0452f249dd5f3b924a476168323/q1.json";
+   /*
+    this.http.get<QuestionSingle[]>(url).subscribe(data =>{
+      this.qList=data;
+    }); */
+    //, {responseType: 'text'}
+    this.http.get(url).subscribe(data => {
       console.log(data);      
       console.log(JSON.stringify(data));      
       this.qList = <QuestionSingle[]>data;
-      console.log(data[0].question);
+      //console.log(data[0].question);
    
     let arrU: QuestionSingle[] = [];
     //Object.assign(arrU, data);
@@ -18,8 +22,18 @@ https://www.concretepage.com/angular-2/angular-httpclient-get-example
     
     //console.log(u.question);
     //console.log(arrU.length);
-    //this.qList.push(u);    
+    //this.qList.push(u);
+    
+  });     
 ---------------------------------------------
+obsBooks: Observable<Book[]>
+getBooks() {
+      this.obsBooks = this.writerService.getBooks();
+} 
+getBooks(): Observable<Book[]> {
+     return this.http.get<Book[]>(this.bookUrl);
+} 
+        
 ---------------------------------------------
 ---------------------------------------------
 ---------------------------------------------
