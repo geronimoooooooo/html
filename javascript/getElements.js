408532f35c256b07d69345869704446676ee3d89
geronimoooooooo <geronimoooooooo@gmail.com>
2022-09-23 14:01:35 +0200

    Create getElements.js

---
+++ javascript/getElements.js
@@ -0,0 +1,11 @@
+<script type="text/javascript">
+    function Validate() {
+        var password = document.getElementById("txtPassword").value;
+        var confirmPassword = document.getElementById("txtConfirmPassword").value;
+        if (password != confirmPassword) {
+            alert("Passwords do not match.");
+            return false;
+        }
+        return true;
+    }
+</script>
