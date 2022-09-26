0fc7a32c7f8c3402703b8aedadd8c28598ba6ba4
geronimoooooooo <geronimoooooooo@gmail.com>
2022-09-26 16:43:32 +0200

    Create examples.js

---
+++ jquery/examples.js
@@ -0,0 +1,16 @@
+$()... shorthand notation/factory function
+jQuery used to select HTML elements
+jQuery settings überschreiben html settings bei Elementen
+
+$(function() {  
+
+  $("p").css("color", "red");  
+
+
+
+if checkbox checked https://www.javatpoint.com/jquery-prop
+
+
+
+
+}); 
