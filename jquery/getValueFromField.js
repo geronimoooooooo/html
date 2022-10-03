5e81ef082db6d5f46c36364d1256848f846a0857
geronimoooooooo <geronimoooooooo@gmail.com>
2022-10-03 15:33:50 +0200

    Create getValueFromField.js

---
+++ jquery/getValueFromField.js
@@ -0,0 +1,3 @@
+jQuery.attr() Vs. jQuery.prop()
+Wie man Wert von einem Label oder input text field rausbekommt und wo anders auf der Seite zeigt
+http://net-informations.com/jq/iq/prop.htm#:~:text=Both%20attr()%20and%20prop,current%20value%20(Current%20state).
