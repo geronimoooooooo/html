b0cd67e6daa42c893ab47b691c10704ddf0fe52c
geronimoooooooo <geronimoooooooo@gmail.com>
2022-11-18 18:36:28 +0100

    Update Node.js

--- Node.js
+++ Node.js
@@ -1,2 +1,4 @@
- Node.js is a runtime environment based on JavaScript.
- Runs the server.
+Node.js is a runtime environment based on JavaScript.
+Runs the server. 
+Windows Installer (.msi) https://nodejs.org/en/download/ 
+npm -v, node -v
