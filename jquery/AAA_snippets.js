32573b05c86c4daf0405b7137c8cf762bbecc090
geronimoooooooo <geronimoooooooo@gmail.com>
2022-11-26 21:33:39 +0100

    Update AAA_snippets.js

--- jquery/AAA_snippets.js
+++ jquery/AAA_snippets.js
@@ -1,6 +1,7 @@
 LISTE mit code snippets
 http://net-informations.com/jq/default.htm
 ----------------------------------------------------------------------------------
+Selektoren in Variable abspeichern: var table = $('.table');
 ----------------------------------------------------------------------------------
 How to Check a jQuery Checkbox is Checked or Not?
 http://net-informations.com/jq/iq/checked.htm
