31e5a4a657565eee277c7c684172d27c9ed4f801
geronimoooooooo <geronimoooooooo@gmail.com>
2023-05-09 06:14:14 +0200

    Delete Node.js

--- Node.js
+++
@@ -1,4 +0,0 @@
-Node.js is a runtime environment based on JavaScript.
-Runs the server. 
-Windows Installer (.msi) https://nodejs.org/en/download/ 
-npm -v, node -v in der cmd wenn unter Systemvariablen "C:\Program Files\nodejs\" vorhanden
