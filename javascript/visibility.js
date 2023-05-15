2cb43f1d04240225d00cb0947aaae7b029505ab9
geronimoooooooo <geronimoooooooo@gmail.com>
2023-05-15 16:16:45 +0200

    Create visibility.js

---
+++ javascript/visibility.js
@@ -0,0 +1,6 @@
+################## JavaScript ##################
+<input type="button" value="Hide text" onclick="document.getElementById('p1').style.visibility='hidden'">
+<input type="button" value="Show text" onclick="document.getElementById('p1').style.visibility='visible'">
+
+
+################## jQuery ##################
