1ee30d77cb5a2753308fb89639909259b6f5ae3f
geronimoooooooo <geronimoooooooo@gmail.com>
2023-05-31 14:47:28 +0200

    Create js.js

---
+++ code-armory/js/js.js
@@ -0,0 +1,22 @@
+onclick = "myFunc()"
+
+conditional operator
+result = (typeof b) =='string' ? "b is string":"b is not a string";
+
+document.getElementById("demo").innerHTML = 
+<button onclick="document.getElementByID("myImg").src ="pic.gif">click</button>
+
+var i;
+var l = arr.lenght;
+for(i=0; i<l; i++)
+  
+try {
+  block of code to try
+  throw new Error("some msg");
+} catch(err){
+  handle error
+} finally{
+  execute regardless of try-catch result
+}
+  
+  
