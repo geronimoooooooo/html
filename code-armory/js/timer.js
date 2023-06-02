04f556933b9945ed502590c8793ec16baa739889
geronimoooooooo <geronimoooooooo@gmail.com>
2023-06-02 14:08:01 +0200

    Update timer.js

--- code-armory/js/timer.js
+++ code-armory/js/timer.js
@@ -1,3 +1,28 @@
+<body onload="startTime()">
+<script type="text/javascript">
+function startTime() {
+	  const today = new Date();
+	  let h = today.getHours();
+	  let m = today.getMinutes();
+	  let s = today.getSeconds();
+	  m = checkTime(m);
+	  s = checkTime(s);
+	  document.getElementById('time').innerHTML =  h + ":" + m + ":" + s;
+	  setTimeout(startTime, 1000);
+	}
+
+function checkTime(i) {
+	  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+	  return i;
+	}
+</script>
+  
+<div class=container-fluid">
+	<div class="row float-end">
+		<p id="time" class="me-3"></p>
+	</div>
+</div>
+################################################
 setInterval()
 setTimeout(function(){
 }, 5000);
