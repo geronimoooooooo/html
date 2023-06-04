7f0725bd581eb730d81a6723bdce6d6871e4f66a
geronimoooooooo <geronimoooooooo@gmail.com>
2023-06-04 22:19:23 +0200

    Update and rename add-class.js to add-class-attr.js

---
+++ code-armory/js/add-class-attr.js
@@ -0,0 +1,18 @@
+############################### CLASS ###########################################
+https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_add_class
+
+<script>
+function myFunction() {
+   var element = document.getElementById("myDIV");
+   element.classList.add("mystyle");
+}
+</script>
+############################### ATTRIBUTE ###########################################
+https://bobbyhadz.com/blog/javascript-set-attribute-disabled
+const button = document.getElementById('btn');
+
+// Set the disabled attribute
+button.setAttribute('disabled', '');
+
+// Remove the disabled attribute
+button.removeAttribute('disabled');
--- code-armory/js/add-class.js
+++
@@ -1,8 +0,0 @@
-https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_add_class
-
-<script>
-function myFunction() {
-   var element = document.getElementById("myDIV");
-   element.classList.add("mystyle");
-}
-</script>
