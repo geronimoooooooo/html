032b9940d354b4c2e2c3f020c44326360787f4d4
geronimoooooooo <geronimoooooooo@gmail.com>
2023-06-04 21:23:41 +0200

    Create add-class.js

---
+++ code-armory/js/add-class.js
@@ -0,0 +1,8 @@
+https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_add_class
+
+<script>
+function myFunction() {
+   var element = document.getElementById("myDIV");
+   element.classList.add("mystyle");
+}
+</script>
