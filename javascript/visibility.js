ee15de9931c3c3b9f6fb4a0669dc64f62f679aca
geronimoooooooo <geronimoooooooo@gmail.com>
2023-07-28 17:22:27 +0200

    Update visibility.js

--- javascript/visibility.js
+++ javascript/visibility.js
@@ -2,6 +2,10 @@
 <input type="button" value="Hide text" onclick="document.getElementById('p1').style.visibility='hidden'">
 <input type="button" value="Show text" onclick="document.getElementById('p1').style.visibility='visible'">
 
+css
+.gone-baby-gone{ display: none;} removes it from your webpage, as if it never existed in your code
+.hollow-man{ visibility: hidden;} only turns it invisible: it still takes up the space it was supposed to
+
 
 ################## jQuery ##################
 var isVisible = $( "#myDiv" ).is( ":visible" );
