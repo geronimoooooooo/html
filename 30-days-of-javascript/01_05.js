31f41e8a2a1c531dd33cec07e8a68a94497f85d0
geronimoooooooo <geronimoooooooo@gmail.com>
2023-11-15 15:30:32 +0100

    Update 01_05.js

--- 30-days-of-javascript/01_05.js
+++ 30-days-of-javascript/01_05.js
@@ -66,7 +66,7 @@ if(){}
 else if(){}
 else(){
 
-termanry operator
+ternary operator
 isBool ? doTrue:doFalse
 
 #####################################################################################
