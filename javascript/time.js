7f3fc4ce96298f17ccaa6efb276f5cc06a37d642
geronimoooooooo <geronimoooooooo@gmail.com>
2024-03-16 00:07:56 +0100

    Update time.js

--- javascript/time.js
+++ javascript/time.js
@@ -8,5 +8,19 @@ text = date+"T"+time;
 document.getElementById("demo").innerHTML = text;  //29.10.2023T16:33:58
 </script>
 
-
 https://www.flypenguin.de/2021/08/18/javascript-iso-date-with-local-timezone/
+
+ISO
+https://stackoverflow.com/questions/34053715/how-to-output-date-in-javascript-in-iso-8601-without-milliseconds-and-with-z
+https://www.freecodecamp.org/news/javascript-get-current-date-todays-date-in-js/
+
+let now = new Date();
+console.log(now.toISOString());
+console.log(now.toISOString().slice(0,-5)+"Z");
+console.log(now.toISOString().replace(/[.]\d+/, ''));
+console.log(now.toISOString().replace('T', ' ').replace(/[.]\d+/, ''));
+
+2024-03-15T23:06:46.534Z
+2024-03-15T23:06:46Z
+2024-03-15T23:06:46Z
+2024-03-15 23:06:46Z
