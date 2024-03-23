9b9e11a8991a98f3328ab3c83eea93d593af1950
geronimoooooooo <geronimoooooooo@gmail.com>
2024-03-23 05:19:16 +0100

    Update async_callback.js

--- javascript/async_callback.js
+++ javascript/async_callback.js
@@ -1,3 +1,4 @@
++ https://javascript.info/async
 + express-async-handler https://www.npmjs.com/package/express-async-handler
 + Vergleiche mit Promise und async/await https://blog.risingstack.com/mastering-async-await-in-nodejs/
 
@@ -21,8 +22,26 @@ const getInfo = async () => {
 }
 
 getInfo();
+###################### callback #######################################
+function loadScript(src, callback) {
+  ...
+  loadScript('/my/script.js', function(error, script) {
+  if (error) {
+    // handle error
+  } else {
+    // script loaded successfully
+  }
+});
 #######################################################################
-#######################################################################
+  let promise = new Promise(function(resolve, reject) {
+  setTimeout(() => reject(new Error("Whoops!")), 1000);
+});
+
+// reject runs the second function in .then
+promise.then(
+  result => alert(result), // doesn't run
+  error => alert(error) // shows "Error: Whoops!" after 1 second
+);
 #######################################################################
 #######################################################################
 #######################################################################
