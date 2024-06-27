c8bec2f322ea8df0ed2fbe17e2b2f6020864a53f
geronimoooooooo <geronimoooooooo@gmail.com>
2024-06-27 17:28:50 +0200

    Update time.js

--- javascript/time.js
+++ javascript/time.js
@@ -34,6 +34,25 @@ console.log(currentDate);
 currentDate.setMinutes(currentDate.getMinutes() - 10)
 console.log(currentDate)
 ########################################################################################
+static showDateTime() {
+    let now = new Date()
+    let year = now.getFullYear()
+    let month = now.getMonth() + 1
+    let date = now.getDate()
+    let hours = now.getHours()
+    let minutes = now.getMinutes()
+    if (hours < 10) {
+      hours = '0' + hours
+    }
+    if (minutes < 10) {
+      minutes = '0' + minutes
+    }
+
+    let dateMonthYear = date + '.' + month + '.' + year
+    let time = hours + ':' + minutes
+    let fullTime = dateMonthYear + ' ' + time
+    return fullTime
+  }
 ########################################################################################
 ########################################################################################
 ########################################################################################
